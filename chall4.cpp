0151229b9a2265d9981b94ef8a047b9cea330b9f
Gregor <ic17b020@technikum-wien.at>
2020-08-03 20:53:53 +0200

    Challenge 4 finished

---
+++ chall4.cpp
@@ -0,0 +1,38 @@
+// Author: Gregor Mersich
+// Date: 03.08.2020
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+uint num;
+bool isPrime(uint q);
+
+int main(){
+    std::cout << "Please enter a number: " << std::endl;
+    std::cin >> num;
+
+    if(num%2 == 0){
+        num -= 1;
+    }
+
+    while(!isPrime(num)){
+        num -= 2;
+    }
+
+    std::cout << "The largest prime smaller than your given number is: " << num << std::endl;
+
+    return(0);
+
+}
+
+bool isPrime(uint q){
+    for(int i = 2; i < q; i+=2){
+        if(!(q%i)){
+            return false;
+        }
+    }
+
+    return true;
+}
