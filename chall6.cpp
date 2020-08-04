aa8991f58c5813252b813cfe2f7da059fc06e2c7
Gregor <ic17b020@technikum-wien.at>
2020-08-04 22:04:08 +0200

    finished challenge 6

---
+++ chall6.cpp
@@ -0,0 +1,37 @@
+// Author: Gregor Mersich
+// Date: 04.08.2020
+
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+
+int limit;
+
+void isAbundant(int num);
+
+int main(){
+    std::cout << "Please enter your limit: ";
+    std::cin >> limit;
+    std::cout << std::left << std::setw(10) << "\nNumber" << std::setw(10) << "Abundance" << std::endl;
+
+    for(int i = 1; i <= limit; i++){
+        isAbundant(i);
+    }
+
+    return(0);
+}
+
+void isAbundant(int num){
+    int divSum = 0;
+    
+    for(int i = 1; i <= (num/2); i++){
+        if(num%i == 0){
+            divSum += i;
+            }
+    }
+
+    if(divSum > num){
+        std::cout << std::left << std::setw(10) << num << std::setw(10) << (divSum - num) << std::endl; 
+    }
+
+}
