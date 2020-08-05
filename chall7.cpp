24d0b05ebff68ea5fbc0816c7d93d2739ec239b5
Gregor <ic17b020@technikum-wien.at>
2020-08-05 18:50:52 +0200

    challenge 7 finished

---
+++ chall7.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+const int limit = 1000000;
+
+int divSum(int num);
+std::map<int, int> numMap;
+
+int main()
+{
+    for (int i = 1; i < limit; i++)
+    {
+        if (numMap.find(i) != numMap.end()) {
+            continue;
+        }
+
+        int a = i;
+        int b = divSum(a);
+
+        if (a == b) {
+            continue;
+        }
+
+        if (a == divSum(b)) {
+            numMap.insert(std::make_pair(b, a));
+            std::cout << a << "   " << b << std::endl;
+        }
+    }
+}
+
+int divSum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= (num / 2); i++)
+    {
+        if (num % i == 0)
+        {
+            sum += i;
+        }
+    }
+    return sum;
+}
